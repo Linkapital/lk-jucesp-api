98f88f01ea0e080482d7cf7b82e25814a90449b4
Osmanis <osmanis@linkapital.com.br>
2023-11-08 15:27:10 -0300

    refctors

--- src/main/java/com/lk/jucesp/bots/MainApplication.java
+++ src/main/java/com/lk/jucesp/bots/MainApplication.java
@@ -1,9 +1,12 @@
 package com.lk.jucesp.bots;
 
+import com.lk.jucesp.bots.components.DocumentMetadata;
 import com.lk.jucesp.bots.components.SPJucespBot;
 import com.lk.jucesp.bots.components.SPJucespBuilderImpl;
 import com.lk.jucesp.bots.exceptions.CannotGetJucespFileException;
 
+import java.io.InputStream;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class MainApplication {
@@ -15,7 +18,9 @@ public class MainApplication {
         if (args.length > 0) {
             String socialReason = args[0];
             SPJucespBot spJucespBot = new SPJucespBot(new SPJucespBuilderImpl());
-            spJucespBot.getArchivedDocuments(socialReason);
+            InputStream find1 = spJucespBot.getRegistrationForm(socialReason);
+            InputStream find2 = spJucespBot.getSimplifiedCertification(socialReason);
+            List<DocumentMetadata> find3 = spJucespBot.getArchivedDocuments(socialReason);
         }
     }
 }
--- src/main/java/com/lk/jucesp/bots/components/SPJucespCredentialsGenerator.java
+++ src/main/java/com/lk/jucesp/bots/components/SPJucespCredentialsGenerator.java
@@ -27,7 +27,6 @@ public class SPJucespCredentialsGenerator {
     private void fillCredentials() {
         this.credentials.add(SPJucespCredentials.builder().cpf("24246324809").password("Cubano1404").build());
         this.credentials.add(SPJucespCredentials.builder().cpf("24302834846").password("Cubana1301").build());
-        this.credentials.add(SPJucespCredentials.builder().cpf("80151528950").password("Mn1234567*").build());
         this.credentials.add(SPJucespCredentials.builder().cpf("80151524963").password("Mn1234567*").build());
         this.credentials.add(SPJucespCredentials.builder().cpf("29674360700").password("TIKUNolan24").build());
     }
