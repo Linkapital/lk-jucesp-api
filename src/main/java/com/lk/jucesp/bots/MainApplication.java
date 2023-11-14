14cde8233b04e6aae46a0542e10772787a20a1eb
Abdiel Matos Nieto <hackerabdi@gmail.com>
2023-11-14 23:01:08 +0300

    Limpieza de codigo en el MainApplication.java(Quitando imports innecesarios)

--- src/main/java/com/lk/jucesp/bots/MainApplication.java
+++ src/main/java/com/lk/jucesp/bots/MainApplication.java
@@ -1,20 +1,12 @@
 package com.lk.jucesp.bots;
 
-import com.lk.jucesp.bots.components.DocumentMetadata;
-import com.lk.jucesp.bots.components.SPJucespBot;
-import com.lk.jucesp.bots.components.SPJucespBuilderImpl;
-import com.lk.jucesp.bots.exceptions.CannotGetJucespFileException;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
 import java.util.logging.Logger;
 
 public class MainApplication {
 
     public static final Logger LOGGER = Logger.getLogger(MainApplication.class.getName());
 
-    public static void main(String[] args) throws CannotGetJucespFileException, IOException {
+    public static void main(String[] args){
         LOGGER.info("Running");
     }
 }
