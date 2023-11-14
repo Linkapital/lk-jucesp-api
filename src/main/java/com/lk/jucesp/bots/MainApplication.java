993137835de9af9c958e6bee3b97018327971d91
Abdiel Matos Nieto <hackerabdi@gmail.com>
2023-11-14 23:00:02 +0300

    Limpieza de codigo en el MainApplication.java

--- src/main/java/com/lk/jucesp/bots/MainApplication.java
+++ src/main/java/com/lk/jucesp/bots/MainApplication.java
@@ -16,18 +16,5 @@ public class MainApplication {
 
     public static void main(String[] args) throws CannotGetJucespFileException, IOException {
         LOGGER.info("Running");
-        if (args.length > 0) {
-            String socialReason = args[0];
-            SPJucespBot spJucespBot = new SPJucespBot(new SPJucespBuilderImpl());
-            LOGGER.info("Getting documents by Social Reason");
-            List<DocumentMetadata> find1 = spJucespBot.getArchivedDocuments(socialReason);
-            LOGGER.info("End for getting documents by Social Reason");
-            LOGGER.info("Getting company registration form by Nire Code");
-            InputStream find2 = spJucespBot.getRegistrationForm(spJucespBot.getNire());
-            LOGGER.info("End of getting company registration form by Nire Code");
-            LOGGER.info("Getting company Simplified Certification by Nire Code");
-            InputStream find3 = spJucespBot.getSimplifiedCertification(spJucespBot.getNire());
-            LOGGER.info("End of getting company Simplified Certification by Nire Code");
-        }
     }
 }
